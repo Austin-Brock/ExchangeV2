c8ae0c90d23daaa15dbb0c5afc4bf3d3c24458c7
Austin Brock <austinbrock1981@yahoo.com>
2023-06-01 18:06:39 -0400

    Initial Commit

---
+++ Exchange/ExchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangeApp.swift
+//  Exchange
+//
+//  Created by Austin Brock on 6/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ExchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
